b8b6c122c8a5ba5beb0a58b510c92744808c1373
mtchen <mingtsechenjustdoit@gmail.com>
2021-03-16 20:54:54 +0800

    first commit

---
+++ AOPSample/ExceptionAspect.cs
@@ -0,0 +1,34 @@
+﻿using PostSharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPSample
+{
+    [Serializable]
+    class ExceptionAspect : OnExceptionAspect
+    {
+        public string Message { get; set; }
+
+        public Type ExceptionType { get; set; }
+
+        public FlowBehavior Behavior { get; set; }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            var msg =$" { DateTime.Now }:{ Message + Environment.NewLine } ";
+            msg += $"{ DateTime.Now }:Error running { args.Method.Name }. { args.Exception.Message }{ Environment.NewLine }{ args.Exception.StackTrace }";
+            Debug.WriteLine(msg);
+            Console.WriteLine(msg);
+            args.FlowBehavior = FlowBehavior.Continue;
+
+            Console.ReadKey();
+        }
+
+        public override Type GetExceptionType(MethodBase targetMethod) => ExceptionType;
+    }
+}
---
+++ AOPSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using PostSharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPSample
+{
+    class Program
+    {
+        [ExceptionAspect(ExceptionType = typeof(Exception), Message = "A Sample Exception Message", Behavior = FlowBehavior.Continue)]
+        static void Main(string[] args)
+        {
+            throw new Exception("Sample Exception");
+        }
+    }
+}
